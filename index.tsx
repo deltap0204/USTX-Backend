dc5b23f0d634987c5a1eb66cb9d3f54b1b7d4fb8
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-02-05 15:16:22 +0530

    implemented cron job

--- index.tsx
+++ index.tsx
@@ -3,6 +3,10 @@ console.log('here');
 require('dotenv').config();
 const http = require('http');
 const app = require('./app.tsx');
+const cron = require('node-cron');
+const FundValueService = require('./services/fund-value.service.ts').FundValueService;
+const ShareValueService = require('./services/share-value.service.ts').ShareValueService;
+const moment = require('moment');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +14,19 @@ console.log('creating server');
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
+
+    const SCHEDULE_MINUTES = process.env.SCHEDULE_MINUTES;
+    const SCHEDULE_HOUR = process.env.SCHEDULE_HOUR;
+
+    cron.schedule(`${SCHEDULE_MINUTES} ${SCHEDULE_HOUR} * * *`, () => {
+        let currentDate = moment()
+        let endingFundValue = FundValueService.getEndingAdjustmentFundValue(currentDate);
+        const shareValue = ShareValueService.getNextDayShareValue(currentDate.clone().add(-1, 'days'));
+        console.log(endingFundValue, shareValue)
+    });
+
     console.log(`Listening on port ${PORT} `)
-});
+},
+);
 
 module.exports=server;
